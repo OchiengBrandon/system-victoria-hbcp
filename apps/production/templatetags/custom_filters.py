560a7085f07cf620442e053382dd07e55920e046
brandondeveloper <brandonochieng72@gmail.com>
2025-03-04 18:06:38 +0300

    Update custom_filters.py

--- apps/production/templatetags/custom_filters.py
+++ apps/production/templatetags/custom_filters.py
@@ -16,3 +16,12 @@ def multiply(value, arg):
 def add_class(field, css_class):
     """Add a CSS class to a form field."""
     return field.as_widget(attrs={'class': css_class})
+
+@register.filter
+def divide(value, arg):
+    """Divide two values."""
+    try:
+        # Ensure both values are converted to Decimal for division
+        return Decimal(value) / Decimal(arg)
+    except (ZeroDivisionError, ValueError, TypeError, InvalidOperation):
+        return 0  # or handle it as you prefer
