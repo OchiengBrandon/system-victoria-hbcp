a9ef0e2d89c7773e26b77ea969977fad464c7266
brandondeveloper <brandonochieng72@gmail.com>
2025-03-04 18:15:35 +0300

    Edits

---
+++ apps/products/templatetags/custom_filters.py
@@ -0,0 +1,25 @@
+from decimal import Decimal, InvalidOperation
+from django import template
+
+register = template.Library()
+
+@register.filter
+def multiply(value, arg):
+    """Multiply two values."""
+    try:
+        return Decimal(value) * Decimal(arg)
+    except (ValueError, TypeError, InvalidOperation):
+        return 0  # or handle it as you prefer
+
+@register.filter
+def divide(value, arg):
+    """Divide two values."""
+    try:
+        return Decimal(value) / Decimal(arg)
+    except (ZeroDivisionError, ValueError, TypeError, InvalidOperation):
+        return 0  # or handle it as you prefer
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    """Add a CSS class to a form field."""
+    return field.as_widget(attrs={'class': css_class})
