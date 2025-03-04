623ea97110632da708f48d4ddc15f405d055d775
brandondeveloper <brandonochieng72@gmail.com>
2025-03-04 18:10:13 +0300

    Update custom_filters.py

--- apps/accounts/templatetags/custom_filters.py
+++ apps/accounts/templatetags/custom_filters.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, InvalidOperation
 from django import template
 
 register = template.Library()
@@ -6,3 +7,12 @@ register = template.Library()
 def add_class(field, css_class):
     """Adds a CSS class to a form field."""
     return field.as_widget(attrs={"class": css_class})
+
+@register.filter
+def divide(value, arg):
+    """Divide two values."""
+    try:
+        # Ensure both values are converted to Decimal for division
+        return Decimal(value) / Decimal(arg)
+    except (ZeroDivisionError, ValueError, TypeError, InvalidOperation):
+        return 0  # or handle it as you prefer
